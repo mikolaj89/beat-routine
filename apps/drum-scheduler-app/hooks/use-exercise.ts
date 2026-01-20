4e1c77d46861c326946fcf016dfa266a7e66dc4d
Mikolaj Wroblewski <mikolaj9891@gmail.com>
2026-01-20 01:21:57 +0100

    feature(mobile) - use metronome in useExercise hook

--- apps/drum-scheduler-app/hooks/use-exercise.ts
+++ apps/drum-scheduler-app/hooks/use-exercise.ts
@@ -2,31 +2,37 @@ import { useState } from 'react';
 import { ExerciseState } from '../components/exercise/exercise-screen/exercise-screen.types';
 import { useTimer } from './use-time-countdown';
 import { getFormattedTime } from '../utils/date-time';
+import { useMetronome } from './use-metronome';
+import { metronomeOptions } from './use-metronome.constants';
 
 export const useExercise = (exerciseDuration: number) => {
   const [mode, setMode] = useState<ExerciseState>('preview');
-
+  const metronome = useMetronome(metronomeOptions);
   const { secondsLeft, startCountdown, stopCountdown, resetCountdown } =
     useTimer(exerciseDuration * 60);
+
+  const isPauseDisabled = mode !== 'active';
+  const isPlayDisabled = mode === 'active';
+  const isPrevNextDisabled = mode !== 'preview';
 
   const timeFormatted = getFormattedTime(secondsLeft);
 
   const startExercise = () => {
     setMode('active');
     startCountdown();
-    // Additional logic to start timer can be added here
+    metronome.play();
   };
 
   const pauseExercise = () => {
     setMode('paused');
     stopCountdown();
-    // Additional logic to pause timer can be added here
+    metronome.stop();
   };
 
   const finishExercise = () => {
     setMode('preview');
     resetCountdown(exerciseDuration * 60);
-    // Additional logic to resume timer can be added here
+    metronome.stop();
   };
 
   return {
@@ -36,5 +42,8 @@ export const useExercise = (exerciseDuration: number) => {
     mode,
     setMode,
     timeFormatted,
+    isPauseDisabled,
+    isPlayDisabled,
+    isPrevNextDisabled,
   };
 };
