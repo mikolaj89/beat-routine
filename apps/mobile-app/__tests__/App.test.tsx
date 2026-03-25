93954515121162ed25d52dfa4e2c1ea8bae11b46
Mikolaj Wroblewski <mikolaj9891@gmail.com>
2026-03-25 12:52:09 +0100

    fix(mobile-app) - fix failing App.test.tsx

--- apps/mobile-app/__tests__/App.test.tsx
+++ apps/mobile-app/__tests__/App.test.tsx
@@ -4,6 +4,16 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
+
+jest.mock('react-native-gesture-handler', () => {
+  const React = require('react');
+
+  return {
+    GestureHandlerRootView: ({ children }: { children: React.ReactNode }) =>
+      children,
+  };
+});
+
 import App from '../App';
 
 test('renders correctly', async () => {
